b0d7a2bad8788185526d169872d423f6203c849e
Markus Iser <markus.iser@kit.edu>
2024-09-02 13:53:59 +0200

    header update

--- minisat/core/ipasir2.cc
+++ minisat/core/ipasir2.cc
@@ -211,8 +211,7 @@ extern "C" {
         return IPASIR2_E_UNSUPPORTED;
     }
 
-    ipasir2_errorcode ipasir2_set_notify(void* solver, void* data, 
-            void (*notify)(void* data, int32_t const* assigned, int32_t const* unassigned)) {
+    ipasir2_errorcode ipasir2_set_fixed(void* solver, void* data, void (*notify)(void* data, int32_t fixed)) {
         return IPASIR2_E_UNSUPPORTED;
     }
 };
